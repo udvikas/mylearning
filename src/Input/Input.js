dd6e51c82327c335e1e4e88311750ddb03f894c6
Vikash Sharma <kvikashsharma09@gmail.com>
2023-05-03 23:36:06 +0530

    Second commit

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -3,14 +3,14 @@ import "./Input.css";
 import Showproduct from "../showProduct/Showproduct";
 
 const Input = () => {
-  const [name, setName] = useState('');
-  const [desc, setDesc] = useState('');
-  const [price, setPrice] = useState('');
-  const [lsize, setLsize] = useState('');
-  const [msize, setMsize] = useState('');
-  const [ssize, setSsize] = useState('');
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [price, setPrice] = useState("");
+  const [lsize, setLsize] = useState("");
+  const [msize, setMsize] = useState("");
+  const [ssize, setSsize] = useState("");
   const [showProduct, setShowProduct] = useState([]);
-   
+
   const nameHandler = (e) => {
     setName(e.target.value);
   };
@@ -29,66 +29,83 @@ const Input = () => {
   const sSizeHandler = (e) => {
     setSsize(e.target.value);
   };
- 
-  const addProductHandler = () => {
-    const product = {name,desc,price,lsize,msize,ssize}
-     setShowProduct([...showProduct, product])
 
-    setName("");
-    setDesc("");
-    setPrice("");
-    setLsize("");
-    setMsize("");
-    setSsize("");
+  const addProductHandler = () => {
+    if (
+      name.length === 0 ||
+      desc.length === 0 ||
+      price.length === 0 ||
+      lsize.length === 0 ||
+      msize.length === 0 ||
+      ssize.length === 0
+    ) {
+      alert("Each Field is Required");
+      setName("");
+      setDesc("");
+      setPrice("");
+      setLsize("");
+      setMsize("");
+      setSsize("");
+    } else {
+      const product = { name, desc, price, lsize, msize, ssize };
+      setShowProduct([...showProduct, product]);
+
+      setName("");
+      setDesc("");
+      setPrice("");
+      setLsize("");
+      setMsize("");
+      setSsize("");
+    }
   };
 
   return (
     <>
-    <div className="topcover">
-      <div>
-        <label htmlFor="">Shoes Name</label>
-        <br />
-        <br />
-        <input type="text" value={name} onChange={nameHandler} />
-      </div>
-      <div>
-        <label htmlFor="">Description</label>
-        <br />
-        <br />
-        <input type="text" value={desc} onChange={descHandler} />
-      </div>
-      <div>
-        <label htmlFor="">Price</label>
-        <br />
-        <br />
-        <input type="number" value={price} onChange={priceHandler} />
-      </div>
-      <div>
-        <label htmlFor="">Quantity Available</label>
-        <div className="qty">
-          <div>
-            <label htmlFor="">L</label>
-            <br />
-            <input type="number" value={lsize} onChange={lSizeHandler} />
-          </div>
-          <div>
-            <label htmlFor="">M</label>
-            <br />
-            <input type="number" value={msize} onChange={mSizeHandler} />
-          </div>
-          <div>
-            <label htmlFor="">S</label>
-            <br />
-            <input type="number" value={ssize} onChange={sSizeHandler} />
+      <div className="topcover">
+        <div>
+          <label htmlFor="">Shoes Name</label>
+          <br />
+          <br />
+          <input type="text" value={name} onChange={nameHandler} />
+        </div>
+        <div>
+          <label htmlFor="">Description</label>
+          <br />
+          <br />
+          <input type="text" value={desc} onChange={descHandler} />
+        </div>
+        <div>
+          <label htmlFor="">Price</label>
+          <br />
+          <br />
+          <input type="number" value={price} onChange={priceHandler} />
+        </div>
+        <div>
+          <label htmlFor="">Quantity Available</label>
+          <div className="qty">
+            <div>
+              <label htmlFor="">L</label>
+              <br />
+              <input type="number" value={lsize} onChange={lSizeHandler} />
+            </div>
+            <div>
+              <label htmlFor="">M</label>
+              <br />
+              <input type="number" value={msize} onChange={mSizeHandler} />
+            </div>
+            <div>
+              <label htmlFor="">S</label>
+              <br />
+              <input type="number" value={ssize} onChange={sSizeHandler} />
+            </div>
           </div>
         </div>
+        <div>
+          <button onClick={addProductHandler}>Add Product</button>
+        </div>
       </div>
-      <div>
-        <button onClick={addProductHandler}>Add Product</button>
-      </div>
-    </div>
-    <Showproduct showProduct={showProduct}/>
-</>
+      <Showproduct showProduct={showProduct} />
+    </>
   );
 };
 
--- src/Navbar/CartButton.js
+++ src/Navbar/CartButton.js
@@ -1,21 +1,31 @@
 import React, { useContext, useState } from "react";
 import "./CartButton.css";
 import { ProfileContext } from "../Context/ContextProvider";
+import { Modal } from "react-overlays";
 
 const CartButton = () => {
-  const { cartCount,cartItems } = useContext(ProfileContext);
-  const [showCart, setShowCart] = useState(false);
+  const { cartCount, cartItems } = useContext(ProfileContext);
+  const [showModal, setShowModal] = useState(false);
 
   const showCartHandler = () => {
-    setShowCart(true);
+    if(cartCount === 0) {
+      alert('Your Cart is Empty! Add atleast One Item')
+      setShowModal(false)
+    } else{
+    setShowModal(true);
+    }
   };
 
   const hideCartHandler = () => {
-    setShowCart(false);
+    setShowModal(false);
   };
-
+  const renderBackdrop = (props) => <div className="backdrop" {...props} />;
+  const placeOrderHandler = () => {
+    alert("Order is Successfully Placed");
+    setShowModal(false)
+  };
   return (
-    <div>
+    <div className="modal-example">
       <button onClick={showCartHandler}>
         <span>
           <i className="fa fa-shopping-cart" aria-hidden="true"></i> Cart{" "}
@@ -24,27 +34,46 @@ const CartButton = () => {
           </sup>
         </span>
       </button>
-      {showCart && (
-        <div className="cart-overlay" onClick={hideCartHandler}>
-          <div className="cart-dialog">
-            <h2>Cart Items</h2>
-            {/* Display the list of cart items here */}
-            {showCart && (
-              <div className="cart-dialog">
-                <h2>Cart Items</h2>
+      <Modal
+        className="modal"
+        show={showModal}
+        onHide={hideCartHandler}
+        renderBackdrop={renderBackdrop}
+      >
+        <div>
+          <div className="modal-header">
+            <div className="modal-title">Your Cart Details</div>
+            <div>
+              <span className="close-button" onClick={hideCartHandler}>
+                x
+              </span>
+            </div>
+          </div>
+          <div className="modal-desc">
+            {showModal && (
+              <div onClick={hideCartHandler}>
                 {cartItems.map((item, index) => (
                   <div key={index}>
-                    <h4>{item.name}</h4>
-                    <p>{item.desc}</p>
-                    <p>${item.price}</p>
-                    <p>Size: {item.lsize || item.msize || item.ssize}</p>
+                    <p>Item: {item.name}</p>
+                    <p>Details: {item.desc}</p>
+                    <p>Price: ${item.price}</p>
+                    <p>Size: {item.lsize && item.msize && item.ssize}</p>
+                    <hr/>
                   </div>
                 ))}
               </div>
             )}
           </div>
+          <div className="modal-footer">
+            <button className="secondary-button" onClick={hideCartHandler}>
+              Close
+            </button>
+            <button className="primary-button" onClick={placeOrderHandler}>
+              Place Order
+            </button>
+          </div>
         </div>
-      )}
+      </Modal>
     </div>
   );
 };
